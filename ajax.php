1ddf5f64295f19b70d8c9e6ba048840be1da9f8b
David Ramirez <dr395@njit.edu>
2018-03-11 20:01:30 -0400

    Finally working code :)

--- ajax.php
+++ ajax.php
@@ -1,23 +1,20 @@
 <?php
-//session_destroy();
+
 session_start();
 include ("newfunctions.php"); //for movieRetrieve function
 ini_set("allow_url_fopen",1);
-$ucopy = $_GET["uu"];
-$ucopy = "$ucopy";
+$zipcode = $_GET["uu"];
+$zipcode = "$zipcode";
 $day = $_GET["u"];
 $day = "$day";
-$_SESSION['zip'] = $ucopy;
+$_SESSION['zip'] = $zipcode;
 $today = date("Y-m-d"); 
 $today = "$today";
 //sleep(2);
-//$url = "http://data.tmsapi.com/v1.1/movies/showings?startDate=$day&zip=$ucopy&api_key=54jmnjmpmgek7ydjy7984zxq"; 
-//http://api.openweathermap.org/data/2.5/weather?zip=$ucopy,us&APPID=29d9e6c71d0cc7ebbe82ad3823d9ed9b
-// http://data.tmsapi.com/v1.1/movies/showings?startDate=2018-02-24&zip=$ucopy&api_key=54jmnjmpmgek7ydjy7984zxq
 
-if(zip_check($ucopy) == false)
+if(zip_check($zipcode) == false || day_check($day) == false)
 {
-$url = "http://data.tmsapi.com/v1.1/movies/showings?startDate=$day&zip=$ucopy&api_key=54jmnjmpmgek7ydjy7984zxq";
+$url = "http://data.tmsapi.com/v1.1/movies/showings?startDate=$day&zip=$zipcode&api_key=54jmnjmpmgek7ydjy7984zxq";
 
 $json = file_get_contents($url);
 $m = json_decode($json, true);
@@ -28,21 +25,57 @@ $genre = array();
 $photoURL = array();
 $purchLink = array();
 for($x = 0; $x < 5; $x++)
-	{
+	{ 
+//	if(isset($m[$x]["title"]))
 	 $title[$x] = $m[$x]["title"];
-//	 echo $title[$x];
-	 $releaseDate[$x] = $m[$x]["releaseDate"];
-	 $genre[$x] = $m[$x]["genres"][0];
-	 $photoURL[$x] = "http://developer.tmsimg.com/"  . $m[$x]["preferredImage"]["uri"] . "?api_key=54jmnjmpmgek7ydjy7984zxq";
-	 $purchLink[$x] =   $m[$x]["showtimes"][0]["ticketURI"];
+	//else {$title[$x] = "hrllo";}
 
+//	if(isset($m[$x]["releaseDate"]))
+	$releaseDate[$x] = $m[$x]["releaseDate"];
+//	else  {$releaseDate[$x] = "help me";}
+	
+//	if(isset($m[$x]["genres"]))	
+	 $genre[$x] = $m[$x]["genres"][0];
+//	else {$genre[$x] = "no genre";}
+
+//	if(isset($m[$x]["preferredImage"]["uri"]))
+	 $photoURL[$x] = "http://developer.tmsimg.com/"  . $m[$x]["preferredImage"]["uri"] . "?api_key=54jmnjmpmgek7ydjy7984zxq";
+		
+//	if(isset($m[$x]["showtimes"][0]["ticketURI"]))
+	 $purchLink[$x] =   $m[$x]["showtimes"][0]["ticketURI"];
+//	else {$purchLink[$x] = "no purch link lul";}
 	}
 for($i = 0; $i < 5; $i++)
 {
-	movieRetrieve($title[$i],$photoURL[$i],$releaseDate[$i],$genre[$i],$purchLink[$i],$day,$ucopy,$url);
+	movieRetrieve($title[$i],$photoURL[$i],$releaseDate[$i],$genre[$i],$purchLink[$i],$day,$zipcode,$url);
 }
 
 }
+
+
+
+ 	$titles = array();
+        $releaseDates = array();
+        $genres = array();
+        $pics = array();
+        $purchLinks = array();
+        $db = mysqli_connect('localhost','root','root','example');
+        mysqli_select_db($db, 'example');
+        $s = "select * from movie where zipcode ='$zipcode' ";
+        $t = mysqli_query($db,$s) or die (mysqli_error($db));
+        while ($r = mysqli_fetch_array($t, MYSQLI_ASSOC))
+        {
+          array_push($titles, $r["title"]);
+          array_push($releaseDates, $r["releaseDate"]);
+          array_push($genres, $r["genre"]);
+          array_push($pics, $r["photo"]);
+          array_push($purchLinks, $r["link"]);
+        }
+
+	$prod = array($titles,$pics, $releaseDates, $genres, $purchLinks);
+echo json_encode($prod);
+
 ?>                                                                 
+
 
 
